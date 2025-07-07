d627a0ca6110c2eb0dc5e73dcdd5a6e2328817ff
naveedah-alt <naveedahadegoke@gmail.com>
2025-07-06 20:48:27 -0400

    Worked on movement, adjusted jumping, cleaned up player script

--- blu_sample/Assets/Scripts/Player.cs
+++ blu_sample/Assets/Scripts/Player.cs
@@ -1,78 +1,59 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.PlayerLoop;
+using Vector3 = UnityEngine.Vector3;
+using Vector2 = UnityEngine.Vector2;
 
 public class Player : MonoBehaviour
 {
-    public Camera camera;
-
     public Rigidbody rBody;
-
+    //Follower gameobject, allows camera to follow Player without being attached to it
     public GameObject follower;
 
     // Fields for Speed
     public float speed;
 
-    public float accelerationRate, decelerationRate;
-
-    // Fields for Turning
-    public float turnSpeed;
-
-    // Fields for Input
+    // Fields for Input (WASD, Joystick, etc)
     public Vector3 movementDirection;
 
-    // Fields for Movement Vectors
-    Vector3 velocity, acceleration;
-
+    //Bool done, checks for movement to be over, particularly useful with jumping, so there is no infinite jump
     bool done;
 
+    //Works in conjunction with done to prevent double jumps
     bool canJump;
 
-    bool canStomp;
-
-    // Fields for Quaternions
-    Quaternion turning;
-
-    Vector3 bufferSpace;
-
-
-    public int terrainLayer = 3;
-    int terrainLayerMask;
-    RaycastHit terrainHit;
-    Vector3 rayOrigin;
-
-    Vector3 m_EulerAngleVelocity;
-
-
-    float forceConrol;
-
+    //Animator that allows for transitions between states
     private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
 
-        bufferSpace = new Vector3(0f, 1f, -1f);
-
     }
 
     // Update is called once per frame
+    /// <summary>
+    /// Changes follower postion every frame update
+    /// </summary>
     void Update()
     {
         follower.transform.position = transform.position;
-        // camera.transform.position = transform.position + bufferSpace;
         anim = GetComponent<Animator>();
     }
 
+    /// <summary>
+    /// Translates gameObject based on InputAction parameter functions below
+    /// </summary>
     void FixedUpdate()
     {
-        transform.Translate(movementDirection * speed * Time.deltaTime);
         if (movementDirection.x != 0f || movementDirection.z != 0f)
         {
+            transform.Translate(movementDirection * speed * Time.deltaTime);
             Debug.Log("is running");
             anim.SetBool("Running", true);
         }
@@ -81,20 +62,21 @@ public class Player : MonoBehaviour
             anim.SetBool("Running", false);
         }
         
-        if (movementDirection.y == 1f)
+        if (movementDirection.y != 0f)
         {
-            if (!done)
+            if (canJump)
             {
-                if (canJump)
-                {
-                    // rBody.AddForce(0f, 5f, 0f, ForceMode.Impulse);
-                    transform.Translate(movementDirection * speed * Time.deltaTime);
-                    done = true;
-                    canJump = false;
-                    canStomp = true;
-                }
+                transform.Translate(movementDirection * speed * Time.deltaTime);
+                canJump = false;
             }
         }
+    }
+
+    public void jump()
+    {
+        Vector3 jumpVector = new Vector3(0f, 3f, 0f);
+        rBody.AddForce(jumpVector, ForceMode.Impulse);
+        // transform.Translate(jumpVector * speed * Time.deltaTime);
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -107,18 +89,10 @@ public class Player : MonoBehaviour
 
     }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.yellow;
-
-        Gizmos.DrawRay(rayOrigin, Vector3.down * 120f);
-    }
-
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        movementDirection = new Vector3(0f, 1f, 0f);
-        done = false;
+        jump();
     }
 
     void OnCollisionStay(Collision collision)
@@ -126,7 +100,6 @@ public class Player : MonoBehaviour
             Debug.Log("collision");
             done = true;
             canJump = true;
-            canStomp = false;
         }
 
 }
