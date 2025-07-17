6cb773a12d0b5b07387b5bc89b09559363fb9525
knn4602 <knn4602@rit.edu>
2025-07-16 23:01:40 -0400

    have added blueberry and blueberry thrower scripts

---
+++ blu_sample/Assets/Scripts/BlueberryThrower.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class BlueberryThrower : MonoBehaviour
+{
+    public BlueberryPool blueberryPool;   
+    public Transform throwPoint;         
+
+    public float throwForce = 10f;        // You can change this in the inspector
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))  // Press space to throw
+        {
+            // Get a blueberry from the pool
+            GameObject bb = blueberryPool.GetBlueberry();
+
+        bb.transform.position = throwPoint.position;
+        bb.transform.rotation = throwPoint.rotation;
+
+        // Tell it which pool it belongs to
+        bb.GetComponent<Blueberry>().Init(blueberryPool);
+
+        // Reset and throw
+        Rigidbody rb = bb.GetComponent<Rigidbody>();
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        rb.AddForce(throwPoint.forward * throwForce, ForceMode.Impulse);
+        }
+    }
+}
