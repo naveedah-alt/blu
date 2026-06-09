66607974d55d8816d36bfacc76c16e77c1ec890a
naveedah-alt <naveedahadegoke@gmail.com>
2026-06-09 16:14:03 -0400

    Patrol semi-perfected
    Patrol does work better, obstacle detection, rotation to avoid/go around them works too. did notice a glitch with the camera, when we move blu and were looking at his head from the top, some weird inverse controls can happen. working on that.
    patrol needs work around obstacles. the movement is very fast when an obstacle is detected but only sometimes. very odd

--- blu_sample/Assets/Scripts/EnemyScript.cs
+++ blu_sample/Assets/Scripts/EnemyScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
@@ -16,9 +17,14 @@ public class EnemyScript : MonoBehaviour
     [SerializeField]
     private bool direction;
     Vector3 origin;
+    [SerializeField]
     int currentIndex;
+    RaycastHit obstacleInFront;
+    Vector3 movingDirection;
     void Start()
     {
+        movingDirection = new Vector3();
+        obstacleInFront = new RaycastHit();
         currentIndex = 0;
         origin = gameObject.transform.position;
         direction = true;
@@ -38,53 +44,210 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
+
+    // private void OnDrawGizmos()
+    // {
+    //     // Set the color with custom alpha.
+    //     Gizmos.color = new Color(0f, 1f, 0f); // Green with custom alpha
+
+    //     // Draw the ray.
+    //     Gizmos.DrawRay(transform.position, Vector3.forward.normalized * 5f);
+
+    //     // Draw a sphere at the end of the ray.
+    //     Gizmos.DrawSphere(transform.position + Vector3.forward.normalized * 5f, 0.1f);
+    // }
 
     void FixedUpdate()
     {
-        if (currentIndex < patrolPoints.Count)
-        {
-            if(direction)
+
+        if(Physics.Raycast(transform.position,movingDirection, out obstacleInFront, 1f))             
+        {  
+            if (obstacleInFront.collider.gameObject.layer != 3 && obstacleInFront.collider.gameObject.layer != 6)                 
             {
-                if (transform.position != patrolPoints[currentIndex])
-                {
-                    patrol(currentIndex);
-                } else
-                {
-                    currentIndex++;
-                }
-            } else
-            {
-                if (transform.position != patrolPointsReverse[currentIndex])
-                {
-                    counterPatrol(currentIndex);
-                } else
-                {
-                    currentIndex++;
-                }
+                Debug.Log("hit something! rotating now!");
+                transform.rotation = 
+                //Quaternion.Inverse(Quaternion.FromToRotation(Vector3.forward, movingDirection));
+                //Quaternion.FromToRotation(movingDirection, Vector3.forward);
+                new Quaternion(gameObject.transform.position.x, 0.0f, 0.0f, 2f);
+                Debug.DrawRay(transform.position, movingDirection, Color.blue); 
             }
         } else
         {
-            currentIndex = 0;
-            if (direction)
+            Debug.DrawRay(transform.position, movingDirection, Color.red);
+            if (currentIndex < patrolPoints.Count)
             {
-                direction = false;
-            } else
-            {
-                direction = true;
-            }
+                if(direction)
+                {
+                    patrol(currentIndex);
+                    // if (transform.position != patrolPoints[currentIndex])
+                    // {
+                    //     patrol(currentIndex);
+                    // } else
+                    // {
+                    //     currentIndex++;
+                    // }
+                } else
+                {
+                    counterPatrol(currentIndex);
+                    // if (transform.position != patrolPointsReverse[currentIndex])
+                    // {
+                    //     counterPatrol(currentIndex);
+                    // } else
+                    // {
+                    //     currentIndex++;
+                    // }
+                }
+            } 
         }
+        // if (currentIndex < patrolPoints.Count)
+        // {
+        //     if(direction)
+        //     {
+        //         patrol(currentIndex);
+        //         // if (transform.position != patrolPoints[currentIndex])
+        //         // {
+        //         //     patrol(currentIndex);
+        //         // } else
+        //         // {
+        //         //     currentIndex++;
+        //         // }
+        //     } else
+        //     {
+        //         counterPatrol(currentIndex);
+        //         // if (transform.position != patrolPointsReverse[currentIndex])
+        //         // {
+        //         //     counterPatrol(currentIndex);
+        //         // } else
+        //         // {
+        //         //     currentIndex++;
+        //         // }
+        //     }
+        // } 
+        // else
+        // {
+        //     currentIndex = 0;
+        //     if (direction)
+        //     {
+        //         direction = false;
+        //     } else
+        //     {
+        //         direction = true;
+        //     }
+        // }
     }
 
     void patrol(int i)
     {  
-        transform.position = Vector3.MoveTowards(gameObject.transform.position, patrolPoints[i], step);   
+        if (i == patrolPoints.Count)
+        {
+            direction = false;
+            //currentIndex = 0;
+        }
+        else {
+            transform.position = Vector3.MoveTowards(gameObject.transform.position, patrolPoints[i], step);
+            movingDirection = (patrolPoints[i]-transform.position).normalized;
+        }   
     }
     
 
     void counterPatrol(int i)
     {
-        transform.position = Vector3.MoveTowards(gameObject.transform.position, patrolPointsReverse[i], step);
+        if (i == patrolPointsReverse.Count)
+        {
+            direction = true;
+            //currentIndex = 0;
+        }
+        else {
+            transform.position = Vector3.MoveTowards(gameObject.transform.position, patrolPointsReverse[i], step);
+            movingDirection = (patrolPointsReverse[i]-transform.position).normalized;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.GetComponent<Trigger>() == true)
+        {
+            if (currentIndex < points.Count)
+            {
+                Debug.Log("incrementing currentindex");
+                currentIndex++;
+            }
+            if (currentIndex >= points.Count)
+            {
+                Debug.Log("zeroing out currentindex");
+                currentIndex = 0;
+            }
+            
+        }
     }
 }
+//         if (collision.collider.gameObject.layer != 3)
+//         {
+//             // transform.rotation = new Quaternion(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z, 0f);
+//             Rigidbody constraints = gameObject.GetComponent<Rigidbody>();
+//             constraints.constraints = RigidbodyConstraints.FreezeRotation;
+
+//             speed *=4;
+
+//             if(direction)
+//             {
+//                 if (transform.position != patrolPoints[currentIndex])
+//                 {
+//                     patrol(currentIndex);
+//                 } else
+//                 {
+//                     currentIndex++;
+//                 }
+//             } else
+//             {
+//                 if (transform.position != patrolPointsReverse[currentIndex])
+//                 {
+//                     counterPatrol(currentIndex);
+//                 } else
+//                 {
+//                     currentIndex++;
+//                 }
+//             }
+//         }
+        
+        
+//     }
+
+//     void OnCollisionExit(Collision collision)
+//     {
+//         if (collision.collider.gameObject.layer != 3)
+//         {
+//             // transform.rotation = new Quaternion(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z, 0f);
+//             Rigidbody constraints = gameObject.GetComponent<Rigidbody>();
+//             constraints.constraints = RigidbodyConstraints.None;
+//             constraints.constraints = RigidbodyConstraints.FreezePositionY;
+//             //constraints.constraints = RigidbodyConstraints.FreezeRotationZ;
+
+            
+
+//             if(direction)
+//             {
+//                 if (transform.position != patrolPoints[currentIndex])
+//                 {
+//                     patrol(currentIndex);
+//                 } else
+//                 {
+//                     currentIndex++;
+//                 }
+//             } else
+//             {
+//                 if (transform.position != patrolPointsReverse[currentIndex])
+//                 {
+//                     counterPatrol(currentIndex);
+//                 } else
+//                 {
+//                     currentIndex++;
+//                 }
+//             }
+
+//             speed /=4;
+//         }
+//     }
+// }
